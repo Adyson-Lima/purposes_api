89a708dcd461918d18d2fdfe8d6736359e26a6cf
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-30 17:44:19 -0300

    27° commit - tdd controller: Consegue excluir um purpose e retornar status 204? GREEN

--- app/controllers/api/v1/purposes_controller.rb
+++ app/controllers/api/v1/purposes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PurposesController < ApplicationController
 
-  before_action :set_purpose, only: %i[show update] # show update destroy
+  before_action :set_purpose, only: %i[show update destroy] # show update destroy
 
   def index
     @purposes = Purpose.all 
@@ -26,6 +26,10 @@ class Api::V1::PurposesController < ApplicationController
     else
       render json: @purpose.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @purpose.destroy!
   end
 
 private 
